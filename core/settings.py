735c7d8b4948590a2951b317f3efa8133a08ea3a
Alfredo Luna Castro <Alfredolunacastro07@gmail.com>
2024-01-18 16:25:22 -0800

    Add django-bootstrap

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Package
+    'bootstrap5',
     # Project
     'tasks',
 ]
