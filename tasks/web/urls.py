3567473b5c52024f07fdef4357dd0c02a1ca5953
Alfredo Luna <alfredo.luna@s4learning.com>
2024-02-06 16:13:32 -0600

    urls protected

--- tasks/web/urls.py
+++ tasks/web/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import TaskCreateView, TaskDeleteView, TaskDetailView, TaskListView, TaskUpdateView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task_list"),
+    path("", login_required(TaskListView.as_view(), login_url="login"), name="task_list"),
     path("create/", login_required(TaskCreateView.as_view(), login_url="login"), name="task_create"),
     path("tasks/<str:pk>/", login_required(TaskDetailView.as_view(), login_url="login"), name="task_detail"),
     path("tasks/<str:pk>/update/", login_required(TaskUpdateView.as_view(), login_url="login"), name="task_update"),
